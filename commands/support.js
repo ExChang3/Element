cc8e526dcd453c4c47adaabf5f90dd23647b4297
David Foster <worshipleader107@gmail.com>
2017-08-14 11:04:11 -0500

    added support

---
+++ commands/support.js
@@ -0,0 +1,6 @@
+module.exports = {
+    main: function(bot, msg) {
+       msg.channel.sendMessage("Here's A Link To Our Support Server: https://discord.gg/BFNMgwH")
+   },
+   help: '*Gives A Link For The Bots Support Server*'
+};
