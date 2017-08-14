b650035826ace8eb41b54e56cb0bdbaef8a40d8f
David Foster <worshipleader107@gmail.com>
2017-08-14 11:01:16 -0500

    added invite

---
+++ commands/invite.js
@@ -0,0 +1,6 @@
+module.exports = {
+    main: function(bot, msg) {
+       msg.channel.sendMessage("if you'd like me to join your server invite me with this link: https://discordapp.com/oauth2/authorize?client_id=346237767795343360&scope=bot&permissions=137415814")
+   },
+   help: '*Sends an invite link for the bot*'
+};
