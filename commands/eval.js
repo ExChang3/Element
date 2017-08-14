5c10a9fb30cef7739d9f50536c51636384927080
ExChang3 <worshipleader107@gmail.com>
2017-08-14 10:48:54 -0500

    added eval

---
+++ commands/eval.js
@@ -0,0 +1,15 @@
+module.exports = {
+  main: (bot, msg) => {
+	if (msg.author.id !== bot.OWNERID) return;
+    try {
+      var output = eval(msg.content)
+      msg.channel.send('**Output:** ' + output)
+      return
+    }
+    catch (error) {
+      msg.channel.send('**Error:** ```' + error + '```')
+      return
+    }
+  },
+  hide: true
+}
